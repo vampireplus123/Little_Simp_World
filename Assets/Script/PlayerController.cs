df8dd4fe3697e59ba216268ba3758a78a0a41b69
vampireplus123 <66988477+vampireplus123@users.noreply.github.com>
2022-08-12 19:16:09 +0700

    Anh Huynh

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody2D rid;
+    private Vector3 moveDir;
+
+    private void Start()
+    {
+        rid = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveY += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveY -= 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX += 1;
+        }
+        moveDir = new Vector3(moveX, moveY).normalized;
+
+        rid.velocity = moveDir * speed;
+    }
+}
